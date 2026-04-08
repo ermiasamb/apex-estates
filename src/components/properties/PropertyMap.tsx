1b532581d88174b0264fb04c5c8ebbd215114508
Ermias Amb <ermiasamb@gmail.com>
2026-04-08 20:05:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/properties/PropertyMap.tsx
+++ src/components/properties/PropertyMap.tsx
@@ -1,12 +1,25 @@
 'use client';
 
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
-import 'leaflet-defaulticon-compatibility';
-
+import L from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import type { Property } from '@/lib/types';
 import { PropertyCard } from './PropertyCard';
+
+// This is the fix for the marker icon issue with Webpack
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
+});
+
 
 interface PropertyMapProps {
   properties: Property[];
